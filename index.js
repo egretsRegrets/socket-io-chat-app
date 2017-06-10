2c971e1c382807edf61b61784abe47bab3764309
mercantilism <samuel.galen.gardner@gmail.com>
2017-06-09 17:40:55 -0700

    serve basic interface

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use('/css', express.static('css'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
